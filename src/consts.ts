3533b3349ee79875d85d136bf9de97d78a65ff7c
Kimmix <kimmix05@gmail.com>
2023-02-07 23:28:39 +0700

    add favicon,image

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tom personal website.';
+export const SITE_TITLE = "Tom's artfolio site";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const BASE_URL = '/tomfutang';
